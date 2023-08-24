b349dc13f783dfbbf0da2ffc0419b94ec9d5fdce
Carlos Largacha <cdll1231@hotmail.com>
2023-08-23 19:35:27 -0500

    added logo and nav bar

--- note-app/src/App.js
+++ note-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <h1>Notes.app</h1>
       </header>
--- note-app/src/components/Nav.jsx
+++ note-app/src/components/Nav.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 function Nav() {
-    return(
-        <nav>
-            
+   return (
+     <div>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                <img src="C:\Users\cdll1\OneDrive\Documentos\GitHub\projects\note-app\src\img\logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+                Notes
+                </a>
+            </div>
         </nav>
-    )
+     </div>
+   );
 }
+
+export default Nav;
