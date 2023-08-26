eccb3133c58f9cd9d75632b3220ddf9ba1b08f02
Carlos Largacha <cdll1231@hotmail.com>
2023-08-25 19:28:55 -0500

    Modified post-it
    Added the option to change the color and close the post-it

--- note-app/src/components/NotesForm.jsx
+++ note-app/src/components/NotesForm.jsx
@@ -1,37 +1,52 @@
 import React, { useState } from "react";
-import Draggable from 'react-draggable';
+import Draggable, {DraggableCore} from 'react-draggable';
 import './notesForm.css';
 
 function NotesForm() {
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
+    const [isOpen, setIsOpen] = useState(true)
+    const [color, setColor] = useState('rgb(255, 234, 117)')
+    const [showColorOptions, setShowColorOptions] = useState(false)
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        setTitle('')
-        setContent('')
+    const handleClose = () => {
+        setIsOpen(false)
     }
+
+    const handleColorChange = (selectedColor) => {
+        setColor(selectedColor)
+        setShowColorOptions(false)
+    }
+
+    const toggleColorOptions = () => {
+        setShowColorOptions(!showColorOptions)
+    }
+
+    const colorOptions = ['rgb(255, 234, 117)', 'rgb(117, 181, 255)', 'rgb(152, 255, 149)', 'white', 'rgb(255, 167, 251)']
+ 
     return (
-        <Draggable handle=".drag-zone" defaultPosition={{ x: 20, y: 20 }}>
-            <div className="note-form">
-                <div className="drag-zone"></div>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        <input type="text" placeholder="Title"
-                        value={title}
-                        onChange={(event) => setTitle(event.target.value)}/>
-                    </label>
-                    <br />
-                    <label>
-                        <textarea value={content} placeholder="Content"
-                        onChange={(event) => setContent(event.target.value)} />
-                    </label>
-                    <br />
-                    <button type="submit">Save</button>
-                    <div className="drag-zone"></div>
-                </form>
-            </div>
-        </Draggable>
+        <>
+            {isOpen && (
+                <Draggable handle=".handle" defaultPosition={{ x: 0, y: 0 }}>
+                <div className="post-it" style={{ backgroundColor: color }}>
+                    <div className="handle">
+                        <button className="options" onClick={handleClose}><i className="bi bi-x"></i></button>
+                        <button className="options" onClick={toggleColorOptions}><i className="bi bi-gear"></i></button>
+                        {showColorOptions && (
+                            <ul>
+                                {colorOptions.map((option, index) => (
+                                    <li key={index}>
+                                        <button className="post-it-color" style={{ backgroundColor: option}}
+                                        onClick={() => handleColorChange(option)}></button>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
+                    <textarea></textarea>
+                    <div className="handle"></div>
+                </div>
+                </Draggable>
+            )}
+        </>
     )
 }
 
